812cd0513bab73bd9faed5ac97b80de3d226c227
abhishekchaurasiya <abhichek2580@gmail.com>
2022-05-11 06:28:03 +0530

    ok

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -3,20 +3,20 @@ const bookModel = require("../models/bookModels");
 const mongoose = require("mongoose")
 const ObjectId = mongoose.Types.ObjectId
 
-const { isValidRequestBody,isValidData, isValidISBN,isValidReleasedAt } = require("../utils/validator");
+const { isValidRequestBody, isValidData, isValidISBN, isValidReleasedAt } = require("../utils/validator");
 
-function isValidObjectId(id){
-     
-    if(ObjectId.isValid(id)){
-        if((String)(new ObjectId(id)) === id)
-            return true;       
+function isValidObjectId(id) {
+
+    if (ObjectId.isValid(id)) {
+        if ((String)(new ObjectId(id)) === id)
+            return true;
         return false;
     }
     return false;
 }
 
 
-const createBook = async function(req,res){
+const createBook = async function (req, res) {
     try {
         const requestBody = req.body;
 
@@ -26,8 +26,8 @@ const createBook = async function(req,res){
 
         let { title, excerpt, userId, ISBN, category, subcategory, releasedAt, } = requestBody;
 
-        if(!isValidData(title)){
-            return res.status(400).send({ status: false, message: "Title is Required" });  
+        if (!isValidData(title)) {
+            return res.status(400).send({ status: false, message: "Title is Required" });
         }
 
         let duplicateTitle = await bookModel.findOne({ title });
@@ -35,25 +35,25 @@ const createBook = async function(req,res){
             return res.status(400).send({ status: false, msg: "Title already exist" });
         }
 
-        if(!isValidData(excerpt)){
-            return res.status(400).send({ status: false, message: "Excerpt is Required" });  
+        if (!isValidData(excerpt)) {
+            return res.status(400).send({ status: false, message: "Excerpt is Required" });
         }
 
-        if(!isValidData(userId)){
-            return res.status(400).send({ status: false, message: "userId is Required" });  
+        if (!isValidData(userId)) {
+            return res.status(400).send({ status: false, message: "userId is Required" });
         }
 
-        if(!isValidObjectId(userId)){
-            return res.status(400).send({ status: false, message: "userId is Invalid" });  
+        if (!isValidObjectId(userId)) {
+            return res.status(400).send({ status: false, message: "userId is Invalid" });
         }
 
-        let userDetails = await userModel.findById({_id:userId });
+        let userDetails = await userModel.findById({ _id: userId });
         if (!userDetails) {
             return res.status(404).send({ status: false, msg: "User does not exists" });
         }
 
-        if(!isValidData(ISBN)){
-            return res.status(400).send({ status: false, message: "ISBN is Required" });  
+        if (!isValidData(ISBN)) {
+            return res.status(400).send({ status: false, message: "ISBN is Required" });
         }
 
         let duplicateISBN = await bookModel.findOne({ ISBN });
@@ -61,48 +61,52 @@ const createBook = async function(req,res){
             return res.status(400).send({ status: false, msg: "ISBN already exist" });
         }
 
-        if(!isValidISBN.test(requestBody.ISBN)){
-            return res.status(400).send({ status: false, message: "ISBN is invalid" });  
+        if (!isValidISBN.test(requestBody.ISBN)) {
+            return res.status(400).send({ status: false, message: "ISBN is invalid" });
         }
 
-        if(!isValidData(category)){
-            return res.status(400).send({ status: false, message: "Category is Required" });  
+        if (!isValidData(category)) {
+            return res.status(400).send({ status: false, message: "Category is Required" });
         }
 
-        if(!isValidData(subcategory)){
-            return res.status(400).send({ status: false, message: "Subcategory is Required" });  
+        if (!isValidData(subcategory)) {
+            return res.status(400).send({ status: false, message: "Subcategory is Required" });
         }
 
-        if(!isValidData(releasedAt)){
-            return res.status(400).send({ status: false, message: "Please Provide the release date of the book" });  
+        if (!isValidData(releasedAt)) {
+            return res.status(400).send({ status: false, message: "Please Provide the release date of the book" });
         }
 
-        if(!isValidReleasedAt.test(requestBody.releasedAt)){
-            return res.status(400).send({ status: false, message: "The Format of the release date should be look like 'YYYY-MM-DD'" });  
+        if (!isValidReleasedAt.test(requestBody.releasedAt)) {
+            return res.status(400).send({ status: false, message: "The Format of the release date should be look like 'YYYY-MM-DD'" });
         }
 
-        let newBook = await bookModel.create(requestBody) 
+        let newBook = await bookModel.create(requestBody)
         res.status(201).send({ status: true, message: "Book is created successfully", data: newBook });
-        
+
     } catch (error) {
         res.status(500).send({ status: false, message: error.message });
     }
 }
 
 
-const getBooks = async function (req,res){
+const getBooks = async function (req, res) {
     try {
         let requestQuery = req.query;
-        let {userId,category,subcategory} = requestQuery
+        let { userId, category, subcategory } = requestQuery
 
-        let findBooks = await bookModel.find({userId,category,subcategory,isDeleted: false }).select({title:1, excerpt:1, userId:1, category:1,releasedAt:1,reviews:1}).sort()
 
-        res.status(200).send({status:true,msg:"All Books",data:findBooks})
+
+        let findBooks = await bookModel.find({ $or: { $in:[ userId, category, subcategory ] } }).select({ title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 }).sort();
+
+
+
+        res.status(200).send({ status: true, msg: "All Books", data: findBooks })
 
 
     } catch (error) {
-      res.status(500).send({ status: false, message: error.message });  
+        res.status(500).send({ status: false, message: error.message });
     }
 }
 
-module.exports = { createBook,getBooks};
+module.exports = { createBook, getBooks };
